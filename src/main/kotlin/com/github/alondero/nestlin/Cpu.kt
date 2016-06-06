e7229f86484afa01f6586d53515c9eda2b7d95ac
Adam Londero <alondero@gmail.com>
2016-06-06 22:26:17 +0100

    Moved Opcodes logic out of the CPU class

--- src/main/kotlin/com/github/alondero/nestlin/Cpu.kt
+++ src/main/kotlin/com/github/alondero/nestlin/Cpu.kt
@@ -1,7 +1,8 @@
 package com.github.alondero.nestlin
 
 import com.github.alondero.nestlin.gamepak.GamePak
-import java.util.*
+
+private const val TEST_ROM_CRC = 0x9e179d92
 
 class Cpu(
         var memory: Memory = Memory(),
@@ -12,14 +13,13 @@ class Cpu(
         var opcodes: Opcodes = Opcodes()
 ) {
     var currentGame: GamePak? = null
-    private val testRomCrc = 0x9e179d92
 
     fun reset() {
         memory.clear()
         var isTestRom = false
         currentGame?.let {
             memory.readCartridge(it)
-            isTestRom = it.crc.value == testRomCrc
+            isTestRom = it.crc.value == TEST_ROM_CRC
         }
 
         processorStatus.reset()
@@ -31,7 +31,6 @@ class Cpu(
         } else {
             registers.programCounter = resetVector()
         }
-        println("Initialised program counter as ${registers.programCounter.toHexString()}")
     }
 
     private fun resetVector() = memory[0xFFFC, 0xFFFD]
@@ -42,8 +41,7 @@ class Cpu(
         val opcode = opcodes[opcodeVal] ?: throw UnhandledOpcodeException(opcodeVal)
 
         opcode?.apply {
-            println("""Opcode ${Integer.toHexString(opcodeVal)}, $this.
-Registers: $registers""")
+            println("Opcode ${Integer.toHexString(opcodeVal)}, $this\nRegisters: $registers")
             this.op(this@Cpu)
         }
     }
@@ -52,85 +50,6 @@ Registers: $registers""")
         memory[(0x100 + (registers.stackPointer.toUnsignedInt() and 0xFF))] = value
         --registers.stackPointer
     }
-}
-
-
-
-class UnhandledOpcodeException(opcodeVal: Int) : Throwable("Opcode ${Integer.toHexString(opcodeVal)} not implemented")
-
-class Opcodes {
-    val map = HashMap<Int, Opcode>()
-
-    init {
-        map[0x00] = Opcode(Opcode.OpcodeType.BRK) {
-            it.processorStatus.breakCommand = true
-            it.registers.programCounter++
-            it.registers.programCounter.toUnsignedInt().apply {
-                it.push((this shr 8).toSignedByte())
-                it.push((this and 0xFF).toSignedByte())
-            }
-            it.push(it.processorStatus.asByte()) // TODO: Correctly set this
-            it.registers.programCounter = it.memory[0xFFFE, 0xFFFF]
-            it.processorStatus.interruptDisable = true
-
-            //  Takes 7 cycles
-        }
-        map[0x20] = Opcode(Opcode.OpcodeType.JSR) {
-            val next = it.memory[it.registers.programCounter++.toUnsignedInt(), it.registers.programCounter++.toUnsignedInt()]
-
-            it.registers.programCounter--.toUnsignedInt().apply {
-                it.memory[this] // TODO: Do we need this?
-                it.push((this shr 8).toSignedByte())
-                it.push((this and 0xFF).toSignedByte())
-                it.registers.programCounter = next
-            }
-            //  TODO: Takes 6 cycles
-        }
-        map[0x78] = Opcode(Opcode.OpcodeType.SEI) {
-            it.processorStatus.interruptDisable = true
-            //  TODO: Takes 2 cycles
-        }
-        map[0xd8] = Opcode(Opcode.OpcodeType.SEI) {
-            it.processorStatus.decimalMode = false
-            //  TODO: Takes 2 cycles
-        }
-        map[0x4c] = Opcode(Opcode.OpcodeType.JMP) {
-            val temp = it.registers.programCounter
-            it.registers.programCounter = it.memory[it.registers.programCounter++.toUnsignedInt(), it.registers.programCounter++.toUnsignedInt()]
-
-            if (it.registers.programCounter == (temp-1).toSignedShort()) {
-                //  TODO: Set idle
-            }
-            //  TODO: Takes 3 cycles
-        }
-        map[0xa2] = Opcode(Opcode.OpcodeType.LDX) {
-            it.registers.indexX = it.memory[it.registers.programCounter++.toUnsignedInt()].apply {
-                it.processorStatus.setFlags(this)
-            }
-
-            //  TODO: Takes 2 cycles
-        }
-        map[0x86] = Opcode(Opcode.OpcodeType.STX) {
-            it.memory[it.memory[it.registers.programCounter++.toUnsignedInt()].toUnsignedInt()] = it.registers.indexX
-            // TODO: Takes 3 cycles
-        }
-    }
-
-    operator fun get(code: Int): Opcode? = map[code]
-}
-
-class Opcode(val type: OpcodeType, val op: (Cpu) -> Unit) {
-
-    enum class OpcodeType {
-        BRK,
-        JSR,
-        SEI,
-        JMP,
-        LDX,
-        STX
-    }
-
-    override fun toString(): String = type.toString()
 }
 
 enum class AddressingMode {
