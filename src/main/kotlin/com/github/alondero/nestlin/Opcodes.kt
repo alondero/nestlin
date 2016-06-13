3aa1ade155b25087f6e023e36f053cc701f17d9a
Adam Londero <alondero@gmail.com>
2016-06-13 18:30:47 +0100

    More opcode handling

--- src/main/kotlin/com/github/alondero/nestlin/Opcodes.kt
+++ src/main/kotlin/com/github/alondero/nestlin/Opcodes.kt
@@ -22,6 +22,7 @@ class Opcodes {
         map[0x81] = storeOp (indirectXAdr()) { it.registers.accumulator } // STA - Store A in M (Relative to X)
         map[0x86] = storeOp (zeroPagedAdr()) { it.registers.indexX } // STX - Store X in M (Zero Paged)
         map[0x8e] = storeOp (absoluteAdr()) { it.registers.indexX } // STX - Store X in M
+        map[0x84] = storeOp (zeroPagedAdr()) { it.registers.indexY } // STY - Store Y in M (Zero Paged)
 
         //  Set and Clear operations
         map[0x18] = setOp { it.processorStatus.carry = false } // CLC - Clear Carry Flag
@@ -29,6 +30,14 @@ class Opcodes {
         map[0x78] = setOp { it.processorStatus.interruptDisable = true } // SEI - Set Interrupt Disable Status
         map[0xd8] = setOp { it.processorStatus.decimalMode = false } // CLD - Clear Decimal mode
         map[0xf8] = setOp { it.processorStatus.decimalMode = true } // SED - Set Decimal mode
+
+        //  Add M to A with Carry
+        map[0x69] = addToA (immediate()) // ADC - Add M to A
+        map[0x61] = addToA (indirectX()) // ADC - Add M to A
+
+        // SBC - Subtract M from A with Borrow
+        map[0xe9] = subtractFromA (immediate())
+        map[0xe1] = subtractFromA (indirectX())
 
         //  Push operations
         map[0x08] = pushOp { it.processorStatus.asByte() } //  PHP - Push Processor Status on Stack
@@ -38,6 +47,7 @@ class Opcodes {
         map[0xb8] = clearOp { it.processorStatus.overflow = false } // CLV - Clear Overflow Flag
 
         //  Operations over Accumulator
+        map[0x05] = opWithA (zeroPaged()) { a, m -> ((a or m) and 0xff) }  //  ORA - "OR" M with A
         map[0x09] = opWithA (immediate()) { a, m -> ((a or m) and 0xff) }  //  ORA - "OR" M with A
         map[0x01] = opWithA (indirectX()) { a, m -> ((a or m) and 0xff) } // ORA - "OR" M with A
         map[0x29] = opWithA (immediate()) { a, m -> (a and m) } // AND - "AND" M with A
@@ -52,12 +62,15 @@ class Opcodes {
         map[0xa1] = load (indirectX()) { registers, mem -> registers.accumulator = mem } // LDA - Load A with M
         map[0xa2] = load (immediate()) { registers, mem -> registers.indexX = mem } // LDX - Load X with M
         map[0xa0] = load (immediate()) { registers, mem -> registers.indexY = mem } // LDY - Load Y with M
+        map[0xa4] = load (zeroPaged()) { registers, mem -> registers.indexY = mem } // LDY - Load Y with M
         map[0xae] = load (absolute()) { registers, mem -> registers.indexX = mem }// LDX - Load X with M
+        map[0xa6] = load (zeroPaged()) { registers, mem -> registers.indexX = mem }// LDX - Load X with M
 
         //  Compare operations
-        map[0xc9] = compareOp { it.accumulator } //  CMP - Compare M and A
-        map[0xc0] = compareOp { it.indexY } // CPY - Compare M and Y
-        map[0xe0] = compareOp { it.indexX } // CPX - Compare M and X
+        map[0xc9] = compareOp (immediate()) { it.accumulator } //  CMP - Compare M and A
+        map[0xc1] = compareOp (indirectX()) { it.accumulator } //  CMP - Compare M and A
+        map[0xc0] = compareOp (immediate()) { it.indexY } // CPY - Compare M and Y
+        map[0xe0] = compareOp (immediate()) { it.indexX } // CPX - Compare M and X
 
         //  Transfer operations
         map[0xa8] = transfer ({it.accumulator}) { r, acc -> r.indexY = acc } //  TAY - Transfer A to Y
@@ -154,46 +167,6 @@ class Opcodes {
                 registers.accumulator = pop()
                 processorStatus.resolveZeroAndNegativeFlags(registers.accumulator)
                 //  TODO: Takes 4 cycles
-            }
-        }
-
-        //  ADC - Add M to A with Carry
-        map[0x69] = Opcode {
-            it.apply {
-                //  No need to test for decimal mode on NES CPU!
-                val next = readByteAtPC()
-                val currentAccumulator = registers.accumulator
-
-                var result = currentAccumulator.toUnsignedInt() + next.toUnsignedInt()
-                if (processorStatus.carry) result++
-
-                registers.accumulator = result.toSignedByte()
-                processorStatus.carry = (result shr 8) != 0
-                processorStatus.overflow = ((currentAccumulator.toUnsignedInt() xor next.toUnsignedInt()) and 0x80 == 0x00) &&
-                        ((currentAccumulator.toUnsignedInt() xor registers.accumulator.toUnsignedInt()) and 0x80 == 0x80)
-                processorStatus.resolveZeroAndNegativeFlags(registers.accumulator)
-
-                // TODO: Takes 2 cycles
-            }
-        }
-
-        // SBC - Subtract M from A with Borrow
-        map[0xe9] = Opcode {
-            it.apply {
-                //  No need to test for decimal mode on NES CPU!
-                val next = readByteAtPC()
-                val currentAccumulator = registers.accumulator
-
-                var result = currentAccumulator.toUnsignedInt() - next.toUnsignedInt()
-                if (!processorStatus.carry) result--
-
-                registers.accumulator = (result and 0xFF).toSignedByte()
-                processorStatus.carry = (result shr 8) == 0
-                processorStatus.overflow = ((currentAccumulator.toUnsignedInt() xor next.toUnsignedInt()) and 0x80 == 0x80) &&
-                        ((currentAccumulator.toUnsignedInt() xor registers.accumulator.toUnsignedInt()) and 0x80 == 0x80)
-                processorStatus.resolveZeroAndNegativeFlags(registers.accumulator)
-
-                //  TODO: Takes 6 cycles
             }
         }
 
@@ -366,10 +339,10 @@ class Opcodes {
         }
     }
 
-    private fun compareOp(op: (Registers) -> Byte) = Opcode {
+    private fun compareOp(mem: (Cpu) -> Byte, op: (Registers) -> Byte) = Opcode {
         it.apply {
             val register = op(it.registers)
-            val mem = readByteAtPC()
+            val mem = mem(it)
             val comparison: Int = register - mem
             processorStatus.apply {
                 negative = comparison.toSignedByte().isBitSet(7)
@@ -388,6 +361,44 @@ class Opcodes {
                 transfer(registers, this)
             }
             // TODO: Takes 2 cycles
+        }
+    }
+
+    private fun addToA(mem: (Cpu) -> Byte) = Opcode {
+        it.apply {
+            //  No need to test for decimal mode on NES CPU!
+            val next = mem(it)
+            val currentAccumulator = registers.accumulator
+
+            var result = currentAccumulator.toUnsignedInt() + next.toUnsignedInt()
+            if (processorStatus.carry) result++
+
+            registers.accumulator = result.toSignedByte()
+            processorStatus.carry = (result shr 8) != 0
+            processorStatus.overflow = ((currentAccumulator.toUnsignedInt() xor next.toUnsignedInt()) and 0x80 == 0x00) &&
+                    ((currentAccumulator.toUnsignedInt() xor registers.accumulator.toUnsignedInt()) and 0x80 == 0x80)
+            processorStatus.resolveZeroAndNegativeFlags(registers.accumulator)
+
+            // TODO: Takes 2 cycles
+        }
+    }
+
+    private fun subtractFromA(mem: (Cpu) -> Byte) = Opcode {
+        it.apply {
+            //  No need to test for decimal mode on NES CPU!
+            val next = mem(it)
+            val currentAccumulator = registers.accumulator
+
+            var result = currentAccumulator.toUnsignedInt() - next.toUnsignedInt()
+            if (!processorStatus.carry) result--
+
+            registers.accumulator = (result and 0xFF).toSignedByte()
+            processorStatus.carry = (result shr 8) == 0
+            processorStatus.overflow = ((currentAccumulator.toUnsignedInt() xor next.toUnsignedInt()) and 0x80 == 0x80) &&
+                    ((currentAccumulator.toUnsignedInt() xor registers.accumulator.toUnsignedInt()) and 0x80 == 0x80)
+            processorStatus.resolveZeroAndNegativeFlags(registers.accumulator)
+
+            //  TODO: Takes 6 cycles
         }
     }
 
--- src/main/kotlin/com/github/alondero/nestlin/log/Logger.kt
+++ src/main/kotlin/com/github/alondero/nestlin/log/Logger.kt
@@ -12,67 +12,74 @@ class Logger {
 
     init {
         opcodeLog[0x00] = {"${nValue()} ${nValue()}  BRK"}
-        opcodeLog[0x20] = {"${it.byte1} ${it.byte2}  JSR $${it.byte2}${it.byte1}"}
-        opcodeLog[0x78] = {"${nValue()} ${nValue()}  SEI"}
-        opcodeLog[0xd8] = {"${nValue()} ${nValue()}  CLD"}
-        opcodeLog[0xf8] = {"${nValue()} ${nValue()}  SED"}
-        opcodeLog[0x4c] = {"${it.byte1} ${it.byte2}  JMP $${it.byte2}${it.byte1}"}
-        opcodeLog[0x86] = {"${it.byte1} ${nValue()}  STX $${it.byte1} = ${format(it.cpu.registers.indexX)}"}
-        opcodeLog[0x8e] = {"${it.byte1} ${it.byte2}  STX $${it.byte2}${it.byte1} = ${format(it.cpu.registers.indexX)}"}
-        opcodeLog[0x8d] = {"${it.byte1} ${it.byte2}  STA $${it.byte2}${it.byte1} = ${format(it.cpu.registers.accumulator)}"}
-        opcodeLog[0x81] = {indirectOp(it, "STA")}
-        opcodeLog[0xea] = {"${nValue()} ${nValue()}  NOP"}
-        opcodeLog[0x38] = {"${nValue()} ${nValue()}  SEC"}
-        opcodeLog[0xb0] = {"${it.byte1} ${nValue()}  BCS $${it.progc}"}
-        opcodeLog[0x18] = {"${nValue()} ${nValue()}  CLC"}
-        opcodeLog[0x90] = {"${it.byte1} ${nValue()}  BCC $${it.progc}"}
-        opcodeLog[0xa9] = {"${it.byte1} ${nValue()}  LDA #$${it.byte1}"}
-        opcodeLog[0xa5] = {"${it.byte1} ${nValue()}  LDA $${it.byte1} = ${format(it.cpu.registers.accumulator)}"}
-        opcodeLog[0xad] = {"${it.byte1} ${it.byte2}  LDA $${it.byte2}${it.byte1} = ${format(it.cpu.registers.indexX)}"}
-        opcodeLog[0xa1] = {indirectOp(it, "LDA")}
-        opcodeLog[0xa2] = {"${it.byte1} ${nValue()}  LDX #$${it.byte1}"}
-        opcodeLog[0xae] = {"${it.byte1} ${it.byte2}  LDX $${it.byte2}${it.byte1} = ${format(it.cpu.registers.indexX)}"}
-        opcodeLog[0xa0] = {"${it.byte1} ${nValue()}  LDY #$${it.byte1}"}
-        opcodeLog[0xf0] = {"${it.byte1} ${nValue()}  BEQ $${it.progc}"}
-        opcodeLog[0xd0] = {"${it.byte1} ${nValue()}  BNE $${it.progc}"}
-        opcodeLog[0x85] = {"${it.byte1} ${nValue()}  STA $${it.byte1} = ${format(it.cpu.registers.accumulator)}"}
-        opcodeLog[0x24] = {"${it.byte1} ${nValue()}  BIT $${it.byte1} = ${format(it.cpu.registers.accumulator)}"}
-        opcodeLog[0x70] = {"${it.byte1} ${nValue()}  BVS $${it.progc}"}
-        opcodeLog[0x50] = {"${it.byte1} ${nValue()}  BVC $${it.progc}"}
-        opcodeLog[0x10] = {"${it.byte1} ${nValue()}  BPL $${it.progc}"}
-        opcodeLog[0x30] = {"${it.byte1} ${nValue()}  BMI $${it.progc}"}
-        opcodeLog[0x60] = {"${nValue()} ${nValue()}  RTS"}
+        opcodeLog[0x01] = {indirectOp(it, "ORA")}
+        opcodeLog[0x05] = {"${it.byte1} ${nValue()}  ORA $${it.byte1} = ${format(it.cpu.registers.indexY)}"}
         opcodeLog[0x08] = {"${nValue()} ${nValue()}  PHP"}
-        opcodeLog[0x48] = {"${nValue()} ${nValue()}  PHA"}
-        opcodeLog[0x68] = {"${nValue()} ${nValue()}  PLA"}
+        opcodeLog[0x09] = {"${it.byte1} ${nValue()}  ORA #$${it.byte1}"}
+        opcodeLog[0x0a] = {"${nValue()} ${nValue()}  ASL A"}
+        opcodeLog[0x10] = {"${it.byte1} ${nValue()}  BPL $${it.progc}"}
+        opcodeLog[0x18] = {"${nValue()} ${nValue()}  CLC"}
+        opcodeLog[0x20] = {"${it.byte1} ${it.byte2}  JSR $${it.byte2}${it.byte1}"}
+        opcodeLog[0x21] = {indirectOp(it, "AND")}
+        opcodeLog[0x24] = {"${it.byte1} ${nValue()}  BIT $${it.byte1} = ${format(it.cpu.registers.accumulator)}"}
         opcodeLog[0x28] = {"${nValue()} ${nValue()}  PLP"}
         opcodeLog[0x29] = {"${it.byte1} ${nValue()}  AND #$${it.byte1}"}
-        opcodeLog[0x09] = {"${it.byte1} ${nValue()}  ORA #$${it.byte1}"}
-        opcodeLog[0x01] = {indirectOp(it, "ORA")}
-        opcodeLog[0x21] = {indirectOp(it, "AND")}
-        opcodeLog[0x41] = {indirectOp(it, "EOR")}
-        opcodeLog[0x49] = {"${it.byte1} ${nValue()}  EOR #$${it.byte1}"}
-        opcodeLog[0xc9] = {"${it.byte1} ${nValue()}  CMP #$${it.byte1}"}
-        opcodeLog[0xc0] = {"${it.byte1} ${nValue()}  CPY #$${it.byte1}"}
-        opcodeLog[0xe0] = {"${it.byte1} ${nValue()}  CPX #$${it.byte1}"}
-        opcodeLog[0xb8] = {"${nValue()} ${nValue()}  CLV"}
-        opcodeLog[0xc8] = {"${nValue()} ${nValue()}  INY"}
-        opcodeLog[0x88] = {"${nValue()} ${nValue()}  DEY"}
-        opcodeLog[0xe8] = {"${nValue()} ${nValue()}  INX"}
-        opcodeLog[0xca] = {"${nValue()} ${nValue()}  DEX"}
-        opcodeLog[0xa8] = {"${nValue()} ${nValue()}  TAY"}
-        opcodeLog[0xaa] = {"${nValue()} ${nValue()}  TAX"}
-        opcodeLog[0x98] = {"${nValue()} ${nValue()}  TYA"}
-        opcodeLog[0x8a] = {"${nValue()} ${nValue()}  TXA"}
-        opcodeLog[0xba] = {"${nValue()} ${nValue()}  TSX"}
-        opcodeLog[0x9a] = {"${nValue()} ${nValue()}  TXS"}
-        opcodeLog[0x69] = {"${it.byte1} ${nValue()}  ADC #$${it.byte1}"}
-        opcodeLog[0xe9] = {"${it.byte1} ${nValue()}  SBC #$${it.byte1}"}
-        opcodeLog[0x40] = {"${nValue()} ${nValue()}  RTI"}
-        opcodeLog[0x4a] = {"${nValue()} ${nValue()}  LSR A"}
-        opcodeLog[0x0a] = {"${nValue()} ${nValue()}  ASL A"}
-        opcodeLog[0x6a] = {"${nValue()} ${nValue()}  ROR A"}
         opcodeLog[0x2a] = {"${nValue()} ${nValue()}  ROL A"}
+        opcodeLog[0x30] = {"${it.byte1} ${nValue()}  BMI $${it.progc}"}
+        opcodeLog[0x38] = {"${nValue()} ${nValue()}  SEC"}
+        opcodeLog[0x40] = {"${nValue()} ${nValue()}  RTI"}
+        opcodeLog[0x41] = {indirectOp(it, "EOR")}
+        opcodeLog[0x48] = {"${nValue()} ${nValue()}  PHA"}
+        opcodeLog[0x49] = {"${it.byte1} ${nValue()}  EOR #$${it.byte1}"}
+        opcodeLog[0x4a] = {"${nValue()} ${nValue()}  LSR A"}
+        opcodeLog[0x4c] = {"${it.byte1} ${it.byte2}  JMP $${it.byte2}${it.byte1}"}
+        opcodeLog[0x50] = {"${it.byte1} ${nValue()}  BVC $${it.progc}"}
+        opcodeLog[0x60] = {"${nValue()} ${nValue()}  RTS"}
+        opcodeLog[0x61] = {indirectOp(it, "ADC")}
+        opcodeLog[0x6a] = {"${nValue()} ${nValue()}  ROR A"}
+        opcodeLog[0x68] = {"${nValue()} ${nValue()}  PLA"}
+        opcodeLog[0x69] = {"${it.byte1} ${nValue()}  ADC #$${it.byte1}"}
+        opcodeLog[0x70] = {"${it.byte1} ${nValue()}  BVS $${it.progc}"}
+        opcodeLog[0x78] = {"${nValue()} ${nValue()}  SEI"}
+        opcodeLog[0x81] = {indirectOp(it, "STA")}
+        opcodeLog[0x84] = {"${it.byte1} ${nValue()}  STY $${it.byte1} = ${format(it.cpu.registers.indexY)}"}
+        opcodeLog[0x85] = {"${it.byte1} ${nValue()}  STA $${it.byte1} = ${format(it.cpu.registers.accumulator)}"}
+        opcodeLog[0x86] = {"${it.byte1} ${nValue()}  STX $${it.byte1} = ${format(it.cpu.registers.indexX)}"}
+        opcodeLog[0x88] = {"${nValue()} ${nValue()}  DEY"}
+        opcodeLog[0x8a] = {"${nValue()} ${nValue()}  TXA"}
+        opcodeLog[0x8d] = {"${it.byte1} ${it.byte2}  STA $${it.byte2}${it.byte1} = ${format(it.cpu.registers.accumulator)}"}
+        opcodeLog[0x8e] = {"${it.byte1} ${it.byte2}  STX $${it.byte2}${it.byte1} = ${format(it.cpu.registers.indexX)}"}
+        opcodeLog[0x90] = {"${it.byte1} ${nValue()}  BCC $${it.progc}"}
+        opcodeLog[0x98] = {"${nValue()} ${nValue()}  TYA"}
+        opcodeLog[0x9a] = {"${nValue()} ${nValue()}  TXS"}
+        opcodeLog[0xa0] = {"${it.byte1} ${nValue()}  LDY #$${it.byte1}"}
+        opcodeLog[0xa1] = {indirectOp(it, "LDA")}
+        opcodeLog[0xa2] = {"${it.byte1} ${nValue()}  LDX #$${it.byte1}"}
+        opcodeLog[0xa4] = {"${it.byte1} ${nValue()}  LDY $${it.byte1} = ${format(it.cpu.registers.indexY)}"}
+        opcodeLog[0xa5] = {"${it.byte1} ${nValue()}  LDA $${it.byte1} = ${format(it.cpu.registers.accumulator)}"}
+        opcodeLog[0xa6] = {"${it.byte1} ${nValue()}  LDX $${it.byte1} = ${format(it.cpu.registers.indexX)}"}
+        opcodeLog[0xa8] = {"${nValue()} ${nValue()}  TAY"}
+        opcodeLog[0xa9] = {"${it.byte1} ${nValue()}  LDA #$${it.byte1}"}
+        opcodeLog[0xaa] = {"${nValue()} ${nValue()}  TAX"}
+        opcodeLog[0xad] = {"${it.byte1} ${it.byte2}  LDA $${it.byte2}${it.byte1} = ${format(it.cpu.registers.indexX)}"}
+        opcodeLog[0xae] = {"${it.byte1} ${it.byte2}  LDX $${it.byte2}${it.byte1} = ${format(it.cpu.registers.indexX)}"}
+        opcodeLog[0xb0] = {"${it.byte1} ${nValue()}  BCS $${it.progc}"}
+        opcodeLog[0xb8] = {"${nValue()} ${nValue()}  CLV"}
+        opcodeLog[0xba] = {"${nValue()} ${nValue()}  TSX"}
+        opcodeLog[0xc0] = {"${it.byte1} ${nValue()}  CPY #$${it.byte1}"}
+        opcodeLog[0xc1] = {indirectOp(it, "CMP")}
+        opcodeLog[0xc8] = {"${nValue()} ${nValue()}  INY"}
+        opcodeLog[0xc9] = {"${it.byte1} ${nValue()}  CMP #$${it.byte1}"}
+        opcodeLog[0xca] = {"${nValue()} ${nValue()}  DEX"}
+        opcodeLog[0xd0] = {"${it.byte1} ${nValue()}  BNE $${it.progc}"}
+        opcodeLog[0xd8] = {"${nValue()} ${nValue()}  CLD"}
+        opcodeLog[0xe0] = {"${it.byte1} ${nValue()}  CPX #$${it.byte1}"}
+        opcodeLog[0xe1] = {indirectOp(it, "SBC")}
+        opcodeLog[0xe8] = {"${nValue()} ${nValue()}  INX"}
+        opcodeLog[0xe9] = {"${it.byte1} ${nValue()}  SBC #$${it.byte1}"}
+        opcodeLog[0xea] = {"${nValue()} ${nValue()}  NOP"}
+        opcodeLog[0xf0] = {"${it.byte1} ${nValue()}  BEQ $${it.progc}"}
+        opcodeLog[0xf8] = {"${nValue()} ${nValue()}  SED"}
     }
 
     private fun indirectOp(args: Arguments, op: String): String {
