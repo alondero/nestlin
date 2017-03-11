cee0ab2a3fb6128e69b584eee486ac5954f26e6c
Adam Londero <alondero@gmail.com>
2017-03-11 15:32:09 +0000

    Upgrading to Kotlin 1.1, refactoring GoldenLogTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,18 +4,16 @@ buildscript {
     repositories {
         mavenCentral()
         gradleScriptKotlin()
-        maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.1.0-rc-91")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.1.0")
     }
 }
 
 repositories {
     mavenCentral()
     gradleScriptKotlin()
-    maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
 }
 
 apply {
@@ -23,7 +21,7 @@ apply {
 }
 
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib:1.1.0-rc-91")
+    compile("org.jetbrains.kotlin:kotlin-stdlib:1.1.0")
     compile("org.apache.commons:commons-compress:+")
     compile("org.tukaani:xz:+")
     compile("no.tornado:tornadofx:+") {
